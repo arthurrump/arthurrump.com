397de1e4c58a0ba88ea269f7bcb2777ca9a96955
Arthur Rump <hello@arthurrump.com>
2019-03-24 16:33:45 +0100

    Move icons and rootfiles to root

--- build.fsx
+++ build.fsx
@@ -8,10 +8,12 @@ open Fake.Core
 open Fake.IO.Globbing.Operators
 open Fake.StaticGen
 
+open System.IO
+
 Target.create "Generate" <| fun _ ->
     StaticSite.fromConfig "https://www.arthurrump.com" ()
-    |> StaticSite.withFilesFromSources (!! "rootfiles/*") id
-    |> StaticSite.withFilesFromSources (!! "icons/*") id
+    |> StaticSite.withFilesFromSources (!! "rootfiles/*") Path.GetFileName
+    |> StaticSite.withFilesFromSources (!! "icons/*") Path.GetFileName
     |> StaticSite.generate "public" (fun _ _ -> "")
 
 Target.runOrDefault "Generate"
