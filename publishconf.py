dbd337f6345b0ad8cc8057406a492495b1026b5e
Arthur Rump <hello@arthurrump.com>
2025-02-09 21:10:05 +0100

    Set SITEURL

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
+SITEURL = "https://arthurrump.com"
 FEED_DOMAIN = SITEURL
 RELATIVE_URLS = False
 
